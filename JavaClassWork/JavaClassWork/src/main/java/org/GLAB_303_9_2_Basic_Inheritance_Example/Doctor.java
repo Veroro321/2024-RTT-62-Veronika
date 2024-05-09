5b60ca81a9b2be9c0a032fd8dc0d2ac5a5baeb1e
Veronika Polishuk <>
2024-05-08 23:23:02 -0700

    no message

---
+++ JavaClassWork/JavaClassWork/src/main/java/org/GLAB_303_9_2_Basic_Inheritance_Example/Doctor.java
@@ -0,0 +1,11 @@
+package org.GLAB_303_9_2_Basic_Inheritance_Example;
+
+public class Doctor {
+        String DoctorName;
+        String Department;
+
+        public void Doctor_Details() {
+            System.out.println("Doctor Details...");
+        }
+
+}
---
+++ JavaClassWork/JavaClassWork/src/main/java/org/GLAB_303_9_2_Basic_Inheritance_Example/Surgeon.java
@@ -0,0 +1,8 @@
+package org.GLAB_303_9_2_Basic_Inheritance_Example;
+
+public class Surgeon extends Doctor {
+    void Surgeon_Details() {
+        System.out.println("Surgeon Detail...");
+        System.out.println(Department = "Cardio");
+    }
+}
